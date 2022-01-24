9d305d46163e0b5244640db5aba81c8e3c7cc34f
Clapfix <ronny.perner@kabelmail.de>
2022-01-24 11:11:16 +0100

    Theorie push() / pop()

--- arr_01.js
+++ arr_01.js
@@ -1,5 +1,24 @@
 /*********   Theorie **********/
 
+// push() / pop()
+// push --> Daten rein ... (+)
+let arr = [];
+output(arr);
+arr.push("Ich");
+output(arr);
+arr.push("bin");
+output(arr);
+arr.push("Mia")
+output(arr);
+
+// pop() --> Daten raus ... (-)
+output(arr.pop());
+output(arr);
+output(arr.pop());
+output(arr);
+output(arr.pop());
+output(arr);
+
 
 /*
 Aufgabe:
@@ -13,25 +32,33 @@ const controls = ["<", "</", ">"];
 const tags = ["html","head","head","body","h1","h1","p","p","body","html"];
 let stack = [];
 
-// Modul: HTML-Synthese | Test
-//output(getHTML());
+// // Modul: HTML-Synthese | Test
+// output(getHTML());
 function getHTML() {
     
     let htmlStr = "";
 
     for (let i = 0; i < tags.length; i++) {
-        // htmlStr += getElement(tags[i],"open");
-        // htmlStr += getElement(tags[i],"close");
+        if (isOpenElement(tags[i])) {
+            htmlStr = htmlStr + getElement(tags[i],"open"); 
+        } else {
+            htmlStr = htmlStr + getElement(tags[i],"close");
+        }
     }
 
     return htmlStr;
 }
 
+function isOpenElement() {
+    return true;
+    
+}
 
-// Modul: Zusammenbau der Elements: <tagStr> ---> Test
-output(getElement(tags[1],"open"));
-output(getElement(tags[1],"close"));
-output(getElement(tags[1],));
+
+// // Modul: Zusammenbau der Elements: <tagStr> ---> Test
+// // output(getElement(tags[1],"open"));
+// // output(getElement(tags[1],"close"));
+// // output(getElement(tags[1],));
 function getElement(tag,op) {
     switch (op) {
         case "open":
@@ -41,6 +68,7 @@ function getElement(tag,op) {
         default:
             return "#!?"
     }
+}
 
 //     if (op == "open") {
 //         return controls[0] + tag + controls[2];
@@ -54,8 +82,8 @@ function getElement(tag,op) {
 // }
 
 
-// Modul: Ausgabe | Test
-// output("hi");
+// // Modul: Ausgabe | Test
+// // output("hi");
 function output(outputData) {
     console.log(outputData);
 }
